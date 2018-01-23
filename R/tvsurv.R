abe48cbc1d58a4a4a9214b9d3ad01b140832f0b0
George Williford <gwilliford@gmail.com>
2018-01-22 20:43:00 -0500

    Clean up formatting. Add line to pass basehaz out.

--- R/tvsurv.R
+++ R/tvsurv.R
@@ -7,23 +7,20 @@ tvsurv <- function(Time, Status, X, beta, w, model) {
   for (i in 1:length(death_point)) {
     event[i] <- sum(Status * as.numeric(Time == death_point[i]))
     if (model == "ph")
-      temp <- sum(as.numeric(Time >= death_point[i]) *
-                    w * drop(coxexp))
+      temp <- sum(as.numeric(Time >= death_point[i]) * w * drop(coxexp))
     if (model == "aft")
-      temp <- sum(as.numeric(Time >= death_point[i]) *
-                    w)
+      temp <- sum(as.numeric(Time >= death_point[i]) * w)
     temp1 <- event[i]
     lambda[i] <- temp1/temp
   }
   HHazard <- numeric()
   for (i in 1:length(Time)) {
-    HHazard[i] <- sum(as.numeric(Time[i] >= death_point) *
-                        lambda)
+    HHazard[i] <- sum(as.numeric(Time[i] >= death_point) * lambda)
     if (Time[i] > max(death_point))
       HHazard[i] <- Inf
     if (Time[i] < min(death_point))
       HHazard[i] <- 0
   }
   survival <- exp(-HHazard)
-  list(survival = survival)
+  list(survival = survival, basehaz = HHazard)
 }
