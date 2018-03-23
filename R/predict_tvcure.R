72f1687b2f160ba4ab97105da2deb888dbcfcd91
George Williford <gwilliford@gmail.com>
2018-03-22 21:19:54 -0400

    Update prediction function. 1: to accomodate the probit link function. 2: to put out three values: baseline survival function, survival if uncured, and survival of population

--- R/predict_tvcure.R
+++ R/predict_tvcure.R
@@ -3,7 +3,7 @@
 #' @param model Model returned from tvcure function.
 #' @param newX Values for covariates in hazard formula (X).
 #' @param nexZ Values for covariates in glm formula (Z). This matrix should not include a constant.
-predict_tvcure <- function(model, newX, newZ, ...) {
+predict_tvcure <- function(model, newX, newZ, link, ...) {
   call <- match.call()
   if (!inherits(model, "tvcure"))
     stop("Model must be a tvcure object")
@@ -14,22 +14,29 @@ predict_tvcure <- function(model, newX, newZ, ...) {
   newZ = cbind(1, newZ)
   if (is.vector(newX))
     newX = as.matrix(newX)
-  uncureprob = exp(model$gamma %*% t(newZ))/(1 + exp(model$gamma %*% t(newZ)))
-  scure = array(0, dim = c(n, nrow(newX)))
-  t = array(0, dim = c(n, nrow(newX)))
+  if (link == "logit") {
+    uncureprob <- exp(model$gamma %*% t(newZ)) /
+                (1 + exp(model$gamma %*% t(newZ)))
+  }
+  if (link == "probit") {
+    uncureprob <- pnorm(model$gamma %*% t(newZ))
+  }
+  suncure = array(0, dim = c(n, nrow(newX)))
+  # t = array(0, dim = c(n, nrow(newX)))
   spop = array(0, dim = c(n, nrow(newX)))
   #if (model == "ph") {
   ebetaX = exp(model$beta %*% t(newX))
   for (i in 1:nrow(newZ)) {
-    scure[, i] = s0 ^ ebetaX[i]
+    suncure[, i] = s0 ^ ebetaX[i]
   }
   for (i in 1:n) {
     for (j in 1:nrow(newX)) {
-      spop[i, j] = uncureprob[j] * scure[i, j] + (1 - uncureprob[j])
+      spop[i, j] = uncureprob[j] * suncure[i, j] + (1 - uncureprob[j])
     }
   }
-  prd = cbind(spop, Time = model$Time)
-  sur = cbind(s0, Time = model$Time)
+  #scure = cbind(scure, Time = model$Time)
+  #spop = cbind(spop, Time = model$Time)
+  #sbase = cbind(s0, Time = model$Time)
   #}
   # if (model == "aft") {
   #   newX = cbind(1, newX)
@@ -45,5 +52,5 @@ predict_tvcure <- function(model, newX, newZ, ...) {
   #   prd = cbind(spop = spop, Time = t)
   #   sur = cbind(s0, Time = t)
   # }
-  structure(list(call = call, newuncureprob = uncureprob, prediction = prd))# sur = sur, class = "predicttvcure"))
+  structure(list(call = call, uncureprob = uncureprob, Time = model$Time, suncure = suncure, spop = spop, sbase = s0, class = "predicttvcure"))
 }
