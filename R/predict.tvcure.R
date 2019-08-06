46a0388b2f8c46f1d9f1537ecc1dc9defbc71468
George Williford <gwilliford@gmail.com>
2019-08-06 02:59:04 -0400

    Clean up predict.tvcure code

--- R/predict.tvcure.R
+++ R/predict.tvcure.R
@@ -3,17 +3,17 @@
 #' @param model Model returned from tvcure function.
 #' @param newX Values for covariates in hazard formula (X).
 #' @param nexZ Values for covariates in glm formula (Z). This matrix should not include a constant.
-predict.tvcure <- function(model, newX, newZ, link, ...) {
+predict.tvcure <- function(model, newX = NULL, newZ = NULL, ...) {
   call <- match.call()
   if (!inherits(model, "tvcure"))
     stop("Model must be a tvcure object")
   s0 = as.matrix(model$Survival, ncol = 1)
-  n = nrow(s0)
+  nobs = nrow(s0)
   if (is.vector(newZ))
     newZ = as.matrix(newZ)
-  newZ = cbind(1, newZ)
   if (is.vector(newX))
     newX = as.matrix(newX)
+  link <- model$link
   if (link == "logit") {
     uncureprob <- exp(model$gamma %*% t(newZ)) /
                 (1 + exp(model$gamma %*% t(newZ)))
@@ -21,36 +21,21 @@ predict.tvcure <- function(model, newX, newZ, link, ...) {
   if (link == "probit") {
     uncureprob <- pnorm(model$gamma %*% t(newZ))
   }
-  suncure = array(0, dim = c(n, nrow(newX)))
-  # t = array(0, dim = c(n, nrow(newX)))
-  spop = array(0, dim = c(n, nrow(newX)))
-  #if (model == "ph") {
+  suncure = array(0, dim = c(nobs, nrow(newX)))
+  spop = array(0, dim = c(nobs, nrow(newX)))
   ebetaX = exp(model$beta %*% t(newX))
   for (i in 1:nrow(newZ)) {
     suncure[, i] = s0^ebetaX[i]
   }
-  for (i in 1:n) {
+  for (i in 1:nobs) {
     for (j in 1:nrow(newX)) {
       spop[i, j] = uncureprob[j] * suncure[i, j] + (1 - uncureprob[j])
     }
   }
-  #scure = cbind(scure, Time = model$Time)
-  #spop = cbind(spop, Time = model$Time)
-  #sbase = cbind(s0, Time = model$Time)
-  #}
-  # if (model == "aft") {
-  #   newX = cbind(1, newX)
-  #   ebetaX = exp(model$beta %*% t(newX))
-  #   for (i in 1:nrow(newX)) {
-  #     t[, i] = ebetaX[i] * exp(model$error)
-  #   }
-  #   for (i in 1:n) {
-  #     for (j in 1:nrow(newX)) {
-  #       spop[i, j] = uncureprob[j] * s0[i] + (1 - uncureprob[j])
-  #     }
-  #   }
-  #   prd = cbind(spop = spop, Time = t)
-  #   sur = cbind(s0, Time = t)
-  # }
-  structure(list(call = call, uncureprob = uncureprob, Time = model$Time, suncure = suncure, spop = spop, sbase = s0), class = "predicttvcure")
+  structure(list(call = call, uncureprob = uncureprob, Time = model$Time,
+                 suncure = suncure, spop = spop, sbase = s0, newZ = newZ,
+                 newX = newX, beta = model$beta, gamma = model$gamma,
+                 link = model$link, vcovb = model$vcovb, vcovg = model$vcovg,
+                 Survival = model$Survival),
+            class = "predicttvcure")
 }
