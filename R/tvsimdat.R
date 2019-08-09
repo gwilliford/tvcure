78d349514d23f5199071e49b6b70540daa70ec8f
gwilliford@gmail.com <gwilliford@gmail.com>
2019-08-09 17:19:59 -0400

    Change to skip variables that aren't in a particular matrix

--- R/tvsimdat.R
+++ R/tvsimdat.R
@@ -1,15 +1,28 @@
-tvXsim <- function(model, variable = "TRT", values) {
+tvsimx <- function(model, variable, values) {
+  # browser()
+  # a <- colnames(model$X %in% model$bnames)
   newX <- apply(model$X[, 2:ncol(model$X)], 2, median)
   newX <- matrix(rep(newX, length(values)), ncol = length(newX), byrow = T)
   colnames(newX) <- model$bnames
-  newX[, variable] <- values
+  if (variable %in% model$bnames) newX[, variable] <- values
   newX
 }
 
-tvZsim <- function(model, variable = "TRT", values) {
+tvsimz <- function(model, variable, values) {
   newZ <- apply(model$Z, 2, median)
   newZ <- matrix(rep(newZ, length(values)), ncol = length(newZ), byrow = T)
   colnames(newZ) <- model$gnames
-  newZ[, variable] <- values
+  if (variable %in% model$gnames) newZ[, variable] <- values
   newZ
 }
+
+# tvZsim <- function(model, variable, values) {
+#   newZ <- apply(model$Z, 2, median)
+#   newZ <- matrix(rep(newZ, length(values)), ncol = length(newZ), byrow = T)
+#   colnames(newZ) <- model$gnames
+#   newZ[, variable] <- values
+#   newZ
+# }
+
+# Todo
+# Update to use more than just the median
