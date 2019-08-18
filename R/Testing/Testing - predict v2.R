e472b0c8ddce7e24f34d3c291ea3eaa454de469c
gwilliford@gmail.com <gwilliford@gmail.com>
2019-08-18 18:24:34 -0400

    Update testing files

--- R/Testing/Testing - predict v2.R
+++ R/Testing/Testing - predict v2.R
@@ -32,13 +32,39 @@ Zmat4 <- tvsimz(pd4, "AGE", seq(-30, 30, 10))
 ### Predictions
 # Binary variable, full model
 testpreda <- predict.tvcure(pd, Xmat, Zmat, CI = F)
-testpredb <- predict.tvcure(pd, Xmat, Zmat, CI =  T, nsims = 100)
+testpredb <- predict.tvcure(pd, Xmat, Zmat, CI = T, nsims = 100)
 # Binary variable, partial model
-testpred2a <- predict.tvcure(pd2, Xmat2, Zmat2, CI =  F)
-testpred2a <- predict.tvcure(pd2, Xmat2, Zmat2, CI =  T, nsims = 100)
+testpred2a <- predict.tvcure(pd2, Xmat2, Zmat2, CI = F)
+testpred2a <- predict.tvcure(pd2, Xmat2, Zmat2, CI = T, nsims = 100)
 # Continuous variable, full model
-testpred3a <- predict.tvcure(pd3, Xmat3, Zmat3, CI =  F)
-testpred3b <- predict.tvcure(pd3, Xmat3, Zmat3, CI =  T, nsims = 100)
+testpred3a <- predict.tvcure(pd3, Xmat3, Zmat3, CI = F)
+testpred3b <- predict.tvcure(pd3, Xmat3, Zmat3, CI = T, nsims = 100)
 # Continuous variable, partial model, no cis
-testpred4a <- predict.tvcure(pd4, Xmat4, Zmat4, CI =  F)
-testpred4b <- predict.tvcure(pd4, Xmat4, Zmat4, CI =  T, nsims = 100)
+testpred4a <- predict.tvcure(pd4, Xmat4, Zmat4, CI = F)
+testpred4b <- predict.tvcure(pd4, Xmat4, Zmat4, CI = T, nsims = 100)
+
+testpreda <- prediction2(pd, "TRT", c(0, 1), "basesurv", CI = T)
+testtestpreda <- prediction2(pd, "TRT", c(0, 1), "spop", CI = T)
+
+testpreda <- prediction2(pd, "TRT", c(0, 1), "basesurv", CI = T)
+testtestpreda <- prediction2(pd, "TRT", c(0, 1), "spop", CI = T)
+
+
+cl <- makeCluster(4, "SOCK"); registerDoSNOW(cl)
+a <- makeCluster(4, "SOCK"); registerDoSNOW(a)
+
+system.time(testpreda <- prediction2(pd, "TRT", c(0, 1), "suncure", CI = T, nsims = 10000))
+stopCluster(cl)
+system.time(testpreda <- prediction2(pd, "TRT", c(0, 1), "suncure", CI = T, nsims = 10000))
+
+
+system.time(testpreda <- prediction2(pd, "TRT", c(0, 1), "suncure", CI = T, nsims = 100))
+
+
+testpreda <- prediction2(pd, "TRT", c(0, 1), "suncure")
+testpreda <- prediction2(pd, "TRT", c(0, 1), "spop", bw = T)
+testpreda <- prediction2(pd, "TRT", c(0, 1), "spop", CI = T, nsims = 100)
+testpreda <- prediction2(pd, "TRT", c(0, 1), "spop", CI = T, nsims = 100, bw = T)
+
+### Test population predictions
+
