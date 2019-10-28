a554c011496ba8aa3945c5de88e3ecd69a84d9be
gwilliford@gmail.com <gwilliford@gmail.com>
2019-10-28 15:57:46 -0400

    Reorganize replication analysis.

--- PaperV9ReplicationAnalysis/LHR20191028.R
+++ PaperV9ReplicationAnalysis/LHR20191028.R
@@ -11,5 +11,11 @@ library(dplyr)
 # options(scipen = 999)
 
 # Load data
-lhr <- read.dta13("C:/Users/gwill/Dropbox/Methods Notes/Survival Analysis/Cure Models Paper/Replication - LHR 2008/lhrIOOct08replication.dta")
+lhr <- read.dta13("C:/Users/gwill/Dropbox/Research/Dissertation/tvcure/PaperV9ReplicationAnalysis/LHR20191028.R")
 lhr <- rename(lhr, replace = c("_st" = "st", "_d" = "event", "_t" = "stop", "_t0" = "start"))
+
+
+a <- tvcure(Surv(start, stop, event) ~  capchange + battletide + index + tie + lndeaths + cfhist, cureform = ~ wernerFIRC + capchange + battletide + thirdpartycfire + index + twodem5 + tie + lndeaths + cfhist + stakes + contiguity, data = lhr, model = "ph", nboot = 100)
+
+# Information - Yes/No
+# Commitment -
