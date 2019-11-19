6c9df50f790b2cd1d409ee76c67b79d75670b58f
gwilliford@gmail.com <gwilliford@gmail.com>
2019-11-18 22:24:25 -0500

    Clean up code

--- R/tvem.R
+++ R/tvem.R
@@ -7,12 +7,10 @@
 
     i <- 1
     while (convergence > eps & i < emmax) {
-      if (link == "logit") {
-        uncureprob <- matrix(exp((gamma) %*% t(Z))/(1 + exp((gamma) %*% t(Z))), ncol = 1) #matrix(glmres$fitted.values,
-      }
-      if (link == "probit") {
+      if (link == "logit")
+        uncureprob <- matrix(exp((gamma) %*% t(Z))/(1 + exp((gamma) %*% t(Z))), ncol = 1)
+      if (link == "probit")
         uncureprob <- matrix(pnorm(gamma %*% t(Z)), ncol = 1)
-      }
       survival <- drop(s^(exp((beta) %*% t(X))))
       w <- Status + (1 - Status) * (uncureprob * survival)/((1 -
           uncureprob) + uncureprob * survival)
@@ -24,7 +22,8 @@
                                                     "family = binomial(link = '", link, "'", ")",
                                                    ")", sep = "")))
         } else {
-          incidence_fit <- eval(parse(text = paste("brglm::brglm","(", "as.integer(w) ~ Z[, -1] + offset(offsetvar),",
+          incidence_fit <- eval(parse(text = paste("brglm::brglm","(",
+                                                   "as.integer(w) ~ Z[, -1] + offset(offsetvar),",
                                                    "family = binomial(link='", link, "'", ")",
                                                    ")", sep = "")))
         }
@@ -57,27 +56,19 @@
         update_beta <- coxit$coefficients
         convergence <- sum(c(update_cureg - gamma, update_beta - beta)^2)
                        + sum((s - update_s)^2)
-        # if (is.na(convergence)) {
-        #   if (is.na(update_cureg)) {
-        #     stop("Infinite coefficient. EM algorithm failed to converge.")
-        #   }
-        #   if (is.na(update_cureb)) {
-        #     stop("Infinite coefficient. EM algorithm failed to converge.")
-        #   }
-        # }
-        #if (i >= 53) browser()
         if (is.infinite(convergence)) stop("EM algorithm failed to converge.")
         gamma <- update_cureg
         beta<- update_beta
         s <- update_s
         basehaz <- update_a$basehaz
         if (link == "logit")
-          uncureprob <- matrix(exp((gamma) %*% t(Z))/(1 + exp((gamma) %*% t(Z))), ncol = 1)#matrix(incidence_fit$fitted.values,
+          uncureprob <- matrix(exp((gamma) %*% t(Z))/(1 + exp((gamma) %*% t(Z))), ncol = 1)
         if (link == "probit")
           uncureprob <- matrix(pnorm(gamma %*% t(Z)), ncol = 1)
         i <- i + 1
       }
     }
     em <- list(incidence_fit = incidence_fit, gamma = gamma, latencyfit = beta,
-               Survival = s, Basehaz = basehaz, uncureprob = uncureprob, tau = convergence, emrun = i)
+               Survival = s, Basehaz = basehaz, uncureprob = uncureprob,
+               tau = convergence, emrun = i)
   }
