ba8a1e41147ac893b6ba42c77d0894eaf22f03c2
George Williford <31017813+gwilliford@users.noreply.github.com>
2020-04-15 05:12:30 -0400

    Amend tvtable.coxph. Create varnames/order function.

--- R/tvtable.coxph.R
+++ R/tvtable.coxph.R
@@ -1,89 +1,93 @@
-b <- function(model, format = c("wide", "long"), qi = c("se", "pvalue", "zscore"), stars = T, digits = 3) {
+tvtable.coxph <- function(model, format = c("wide", "long"),
+                          qi = c("se", "pvalue", "zscore"), stars = T, digits = 3,
+                          varlist = NULL)
+{
+  format <- match.arg(format)
   beta  <- round(coef(model), digits)
   bse   <- round(sqrt(diag(model$var)), digits)
   bz    <- round(beta/bse, digits)
   bpval <- round((1 - pnorm(abs(bz))) * 2, digits)
+
+  # Variable names
   bnames <- attr(terms(model), "term.labels")
   allnames <- bnames
-  nobs = nrow(model$y)
-  nfail = model$nevent
-  qi <- match.arg(qi)
-  if (stars == T) {
-    bstar <- gtools::stars.pval(bpval)
-  } else {
-    bstar <- NULL
+  if (is.null(varlist)) varnames <- allnames
+  else {
+    i2 <- match(varlist, allnames)
+    allnames <- c(allnames[i2], allnames[-i2])
+    names(varlist)[names(varlist) == ""] <- varlist[names(varlist) == ""]
+    varnames <- allnames
+    varnames[allnames %in% varlist] <- names(varlist)
   }
 
-emat <- matrix(ncol = 2, nrow = 2)
-emat[ , 1] <- c(nobs, nfail)
-format <- match.arg(format)
 
-bsevec <- vector(length = length(beta))
-bzvec <- vector(length = length(beta))
-bpvalvec <- vector(length = length(beta))
-for (i in 1:length(beta)) {
-  bsevec[i]   <- capture.output(cat("(", bse[i], ")", bstar[i], sep = ""))
-  bzvec[i]    <- capture.output(cat("(", bz[i], ")", bstar[i], sep = ""))
-  bpvalvec[i] <- capture.output(cat("(", bpval[i], ")", bstar[i], sep = ""))
-}
+  # Summary stats
+  nobs = nrow(model$y)
+  nfail = model$nevent
+  emat <- matrix(ncol = 3, nrow = 2)
+  emat[ , 1] <- c("Number of Observations", "Number of Failures")
+  emat[ , 2] <- c(nobs, nfail)
 
-if (qi == "se") {
-  bmat <- as.data.frame(rbind(beta, bsevec))
-}
-if (qi == "zscore") {
-  bmat <- as.data.frame(rbind(beta, bzvec))
-}
-if (qi == "pvalue") {
-  bmat <- as.data.frame(rbind(beta, bpval))
-}
+  # Beta Coefficients and QI
+  qi <- match.arg(qi)
+  if (stars == T) bstar <- gtools::stars.pval(bpval)
+  else bstar <- NULL
+  bsevec <- vector(length = length(beta))
+  bzvec <- vector(length = length(beta))
+  bpvalvec <- vector(length = length(beta))
+  for (i in 1:length(beta)) {
+    bsevec[i]   <- capture.output(cat("(", bse[i], ")", bstar[i], sep = ""))
+    bzvec[i]    <- capture.output(cat("(", bz[i], ")", bstar[i], sep = ""))
+    bpvalvec[i] <- capture.output(cat("(", bpval[i], ")", bstar[i], sep = ""))
+  }
+  if (qi == "se") {
+    bmat <- as.data.frame(rbind(beta, bsevec))
+  }
+  if (qi == "zscore") {
+    bmat <- as.data.frame(rbind(beta, bzvec))
+  }
+  if (qi == "pvalue") {
+    bmat <- as.data.frame(rbind(beta, bpval))
+  }
+  browser()
 
-colnames(bmat) <- bnames
-fullmat <- bmat
-fullmat <- t(fullmat)
-fullmat <- fullmat[allnames, ]
-fullmat <- rbind(as.matrix(fullmat), emat)
-rownames(fullmat)[nrow(fullmat) - 1] <- "Number of Observations"
-rownames(fullmat)[nrow(fullmat)] <- "Number of Failures"
-browser()
-if (qi == "se") {
-  colnames(fullmat) <- c("Hazard Coef.", "Std. Error")
-}
-if (qi == "pvalue") {
-  colnames(fullmat) <- c("Hazard Coef.", "P-value")
-}
-if (qi == "zscore") {
-  colnames(fullmat) <- c("Hazard Coef.", "Z-score")
-}
-for (i in 1:nrow(fullmat) - 2) {
-  fullmat[i, 1] <- ifelse(is.na(fullmat[i, 1]), "", fullmat[i, 1])
-  fullmat[i, 2] <- ifelse(is.na(fullmat[i, 2]), "", fullmat[i, 2])
-}
+  # Combine matrices
+  # colnames(bmat) <- bnames
+  # fullmat <- bmat
+  fullmat <- t(bmat)
+  # fullmat <- fullmat[allnames, ]
+  # rownames(fullmat)[nrow(fullmat) - 1] <- "Number of Observations"
+  # rownames(fullmat)[nrow(fullmat)] <- "Number of Failures"
+  fullmat <- fullmat[allnames, ]
+  fullmat <- cbind(varnames, fullmat)
+  fullmat <- rbind(as.matrix(fullmat), emat)
+  rownames(fullmat) <- NULL
+  if (qi == "se") {
+    colnames(fullmat) <- c("", "Hazard Coef.", "Std. Error")
+  }
+  if (qi == "pvalue") {
+    colnames(fullmat) <- c("", "Hazard Coef.", "P-value")
+  }
+  if (qi == "zscore") {
+    colnames(fullmat) <- c("", "Hazard Coef.", "Z-score")
+  }
+  for (i in 1:nrow(fullmat) - 2) {
+    fullmat[i, 1] <- ifelse(is.na(fullmat[i, 1]), "", fullmat[i, 1])
+    fullmat[i, 2] <- ifelse(is.na(fullmat[i, 2]), "", fullmat[i, 2])
+  }
 
-long1 <- as.vector(rbind(fullmat[, 1], fullmat[, 2]))
-long1 <- long1[1:(length(long1) - 4)]
-long <- long1
-long <- matrix(long, ncol = 1, nrow = length(long1))
-long <- rbind(long, emat[1, 1], emat[2, 1])
-colnames(long) <- "Hazard Coef."
-longnames <- as.vector(rbind(rownames(fullmat), rep("", length(allnames) + 2)))
-rownames(long) <- longnames[-c(length(longnames) - 2, length(longnames))]
-# long <- long[-c(nrow(long), nrow(long) - 2), ]
+  # Create long matrix
+  if (format == "long") {
+    long <- as.vector(rbind(fullmat[, 2], fullmat[, 3]))
+    long <- long[1:(length(long) - 4)]
+    long <- matrix(long, ncol = 1, nrow = length(long))
+    long <- rbind(long, emat[1, 2], emat[2, 2])
+    longnames <- as.vector(rbind(varnames, rep("", length(varnames))))
+    long <- cbind(c(longnames, "Number of Obs.", "Number of Failures"), long)
+    colnames(long) <- c("", "Hazard Coef.")
+  }
 
-# stacked <- as.matrix(c("", long1, "", long2[3:length(long2)], emat[, 1:2]))
-# # stackmat <- matrix(ncol = 1, nrow = length(stacked) + 2)
-# stackmat <- as.data.frame(stacked[1:(nrow(stacked) - 2), 1])
-# # colnames(stacked) <- c("Incidence Coef.", "Hazard Coef.")
-# stacknames <-
-# rownames(stackmat) <- c("Incidence Coef.",
-#                         as.vector(rbind(rownames(fullmat), rep("", length(allnames)))),
-#                         "Hazard Coef.",
-#                         as.vector(rbind(rownames(fullmat)[2:length(allnames)],
-#                         rep("", length(allnames) - 1))), "Number of Observations",
-#                         "Number of Failures")
-# stacknames <- c("Incidence Coefficients", longnames[1:(nrow(long) - 4)], "Hazard Coefficients", longnames[1:(nrow(long) - 4)], "Number of Observations", "Number of Failures")
-
-if (format == "wide")    return(fullmat)
-if (format == "long")    return(long)
-if (format == "stacked") return(stackmat)
+  if (format == "wide") return(fullmat)
+  if (format == "long") return(long)
 }
 
