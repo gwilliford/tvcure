30e3436b5f866a00e724a8a1ade04e564c426855
George Williford <31017813+gwilliford@users.noreply.github.com>
2020-04-29 00:20:26 -0400

    Update V10 Replication.R
    Add table code

--- V10 Replication/LHR20200409.R
+++ V10 Replication/LHR20200409.R
@@ -1,8 +1,9 @@
-setwd("C:/Users/gwill/Dropbox/Research/Dissertation/Article 1 - Cure Models/V10 Replication")
+setwd("C:/Users/gwill/Dropbox/Research/Dissertation/tvcure/V10 Replication")
 library(haven)
 library(dplyr)
 library(tvcure)
 library(matrixStats)
+library(xtable)
 options(scipen = 999)
 cl <- makeCluster(4, "SOCK"); registerDoSNOW(cl)
 
@@ -12,8 +13,14 @@ lhr <- rename(lhr, "st" = "_st", "event" = "_d", "stop" = "_t", "start" = "_t0")
 
 ##### Models ---------------------------------------------------------------------------
 
+
+##### set.seed
+
 # Cox model - full
-cox <- coxph(Surv(start, stop, event) ~ lndeaths + tie + battletide + thirdpartycfire + archigosFIRC + stakes + onedem5 + twodem5 + index + cfhist + contiguity + capchange, data = lhr); summary(cox)
+cox <- coxph(Surv(start, stop, event) ~ lndeaths + tie + battletide + thirdpartycfire +
+               archigosFIRC + stakes + onedem5 + twodem5 + index + cfhist +
+               contiguity + capchange, data = lhr, x = T); summary(cox)
+saveRDS(coxtab, file = "./res/coxtab.RDS")
 
 # Cure model - full
 curefull <- tvcure(Surv(start, stop, event) ~  capchange + lndeaths +
@@ -22,7 +29,9 @@ curefull <- tvcure(Surv(start, stop, event) ~  capchange + lndeaths +
              cureform = ~ archigosFIRC + capchange + lndeaths + battletide +
                tie + thirdpartycfire + stakes + onedem5 + twodem5 + index +
                cfhist + contiguity,
-             data = lhr, var = T, nboot = 100, brglm = T); summary(curefull)
+             data = lhr, var = T, nboot = 1000, brglm = T); summary(curefull)
+saveRDS(curefull, file = "./res/curefull.RDS")
+
 
 # Cure model - partial
 curepart <- tvcure(Surv(start, stop, event) ~  capchange + index +
@@ -30,4 +39,203 @@ curepart <- tvcure(Surv(start, stop, event) ~  capchange + index +
              cureform = ~ archigosFIRC + capchange + lndeaths + battletide +
                tie + thirdpartycfire + stakes + onedem5 + twodem5 + index +
                cfhist + contiguity,
-             data = lhr, var = T, nboot = 30, brglm = T); summary(m1)
+             data = lhr, var = T, nboot = 1000, brglm = T); summary(curepart)
+saveRDS(curepart, file = "./res/curepart.RDS")
+
+
+##### Tables --------------------------------------------------------------------
+vl <- list("Battle Deaths" = "lndeaths",
+     "Tie" = "tie",
+     "Battle Tide" = "battletide",
+     "Third Party Intervention" = "thirdpartycfire",
+     "Capability Change" = "capchange",
+     "Existential Stakes" = "stakes",
+     "Agreement Strength" = "index",
+     "Foreign-Imposed Regime Change" = "archigosFIRC",
+     "Mixed Regime Type" = "onedem5",
+     "Joint Democracy" = "twodem5",
+     "Conflict History" = "cfhist",
+     "Contiguity" = "contiguity")
+parttab <- tvtable_tvcure(curepart, format = "long", varlist = vl)
+fulltab <- tvtable_tvcure(curefull, format = "long", varlist = vl)
+coxtab <- tvtable_coxph(cox, format = "long", varlist = vl)
+
+
+
+# Combined Table
+combtab <- tvtable_combine(c("coxtab", "parttab"))
+print(xtable(combtab, align = "llXXX"),
+      booktabs = T,
+      tabular.environment = "tabularx",
+      width="\\textwidth",
+      include.rownames = F,
+      sanitize.text.function=identity,
+      hline.after = getOption("xtable.hline.after", c(-1,0,nrow(x))),
+) #file = "./tab/combtab.tex")
+combtab2 <- tvtable_combine(c("fulltab", "parttab"), modnum = T)
+print(xtable(combtab2, align = "llXXXX"),
+      booktabs = T,
+      tabular.environment = "tabularx",
+      width="\\textwidth",
+      include.rownames = F,
+      sanitize.text.function=identity,
+      hline.after = getOption("xtable.hline.after", c(-1,0,nrow(x))),
+) #file = "./tab/combtab.tex")
+combtab3 <- tvtable_combine(c("coxtab", "fulltab", "parttab"), modnum = T)
+print(xtable(combtab3, align = "llXXXXX"),
+      booktabs = T,
+      tabular.environment = "tabularx",
+      width="\\textwidth",
+      include.rownames = F,
+      sanitize.text.function=identity,
+      hline.after = getOption("xtable.hline.after", c(-1,0,nrow(x))),
+) #file = "./tab/combtab.tex")
+
+
+# Footnote - pvalues/FIRC stands for foreign imposed regime change.
+
+# Good list
+tvtable.coxph(cox, format = "long",
+        varlist = list("Battle Deaths" = "lndeaths",
+                       "Tie" = "tie",
+                       "Battle Tide" = "battletide",
+                       "Third Party Intervention" = "thirdpartycfire",
+                       "Capability Change" = "capchange",
+                       "Existential Stakes" = "stakes",
+                       "Agreement Strength" = "index",
+                       "Foreign-Imposed Regime Change" = "archigosFIRC",
+                       "Mixed Regime Type" = "onedem5",
+                       "Joint Democracy" = "twodem5",
+                       "Conflict History" = "cfhist",
+                       "Contiguity" = "contiguity")
+)
+
+
+
+
+# Bad List
+tvtable.coxph(cox, varlist = list("$\\ln$ Battle Deaths" = "lndeaths",
+  "Tie" = "tie",
+  "Battle Tide" = "battletide",
+  "Third Party Intervention" = "thirdpartycfire",
+  "Capability Change",
+  "Existential Stakes" = "stakes",
+  "Agreement Strength" = "agree",
+  "Mixed Regime Type" = "onedem5",
+  "Joint Democracy" = "twodem5",
+  "Conflict History" = "cfhist",
+  "Contiguity" = "contiguity",
+  "Intercept"))
+
+# Shortlist
+tvtable.coxph(cox, format = "long",
+              varlist = list("Battle Deaths" = "lndeaths",
+                             "Tie" = "tie",
+                             "Battle Tide" = "battletide",
+                             "Foreign-Imposed Regime Change" = "archigosFIRC",
+                             "Mixed Regime Type" = "onedem5",
+                             "Joint Democracy" = "twodem5",
+                             "Conflict History" = "cfhist",
+                             "Contiguity" = "contiguity")
+)
+
+
+# Good list
+tvtable(curepart, format = "long",
+              varlist = list("Battle Deaths" = "lndeaths",
+                             "Tie" = "tie",
+                             "Battle Tide" = "battletide",
+                             "Third Party Intervention" = "thirdpartycfire",
+                             "Capability Change" = "capchange",
+                             "Existential Stakes" = "stakes",
+                             "Agreement Strength" = "index",
+                             "Foreign-Imposed Regime Change" = "archigosFIRC",
+                             "Mixed Regime Type" = "onedem5",
+                             "Joint Democracy" = "twodem5",
+                             "Conflict History" = "cfhist",
+                             "Contiguity" = "contiguity")
+)
+
+# Bad list
+tvtable(curepart, format = "long",
+        varlist = list("$\\ln$ Battle Deaths" = "lndeaths",
+                       "Tie" = "tie",
+                       "Battle Tide" = "battletide",
+                       "Third Party Intervention" = "thirdpartycfire",
+                       "Capability Change",
+                       "Existential Stakes" = "stakes",
+                       "Agreement Strength" = "agree",
+                       "Mixed Regime Type" = "onedem5",
+                       "Joint Democracy" = "twodem5",
+                       "Conflict History" = "cfhist",
+                       "Contiguity" = "contiguity",
+                       "Intercept")
+)
+
+
+# Bad list 2
+tvtable(curepart, format = "long",
+        varlist = list("Battle Deaths" = "lndeaths",
+                       "Tie" = "tie",
+                       "Battle Tide" = "battletide",
+                       "Third Party Intervention" = "thirdpartycfire",
+                       "Capability Change" = "capchange",
+                       "Existential Stakes" = "stakes",
+                       "Agreement Strength" = "index",
+                       "Foreign-Imposed Regime Change" = "archigosFIRC",
+                       "Mixed Regime Type" = "onedem5",
+                       "Joint Democracy" = "twodem5",
+                       "Conflict History" = "cfhist",
+                       "Contiguity" = "contiguity",
+                       "alpha" = "alpha")
+)
+
+tvtable(curepart, format = "long",
+        varlist = list("$\\ln$ Battle Deaths" = "lndeaths",
+                       "Tie" = "tie",
+                       "Battle Tide" = "battletide",
+                       "Third Party Intervention" = "thirdpartycfire",
+                       "Capability Change",
+                       "Existential Stakes" = "stakes",
+                       "Agreement Strength" = "agree",
+                       "Mixed Regime Type" = "onedem5",
+                       "Joint Democracy" = "twodem5",
+                       "Conflict History" = "cfhist",
+                       "Contiguity" = "contiguity",
+                       "Intercept")
+)
+
+newdata <- apply(cox$x, 2, median, na.rm = T)
+newdata <- as.data.frame(rbind(newdata, newdata))
+newdata[2, "tie"] <- 1
+pcox <- survfit(cox, newdata, conf.int = 0.90)
+plot(pcox, T, col = c(1, 2))
+# pcox <- predict(cox, type = "expected", data = lhr)
+# scox <- exp(-pcox)
+# lattice(x = lhr$stop, x = )
+# library(survminer)
+ggsurvplot(cox, newdata)
+newdata2 <- as.data.frame(rbind(newdata, newdata))
+newdata2[2, "twodem5"] <- 1
+pcox <- survfit(cox, newdata2, conf.int = 0.90)
+plot(pcox, T, col = c(1, 2))
+
+ppart <- prediction3(curefull, "twodem5", c(0, 1), "uncureprob")
+plot(ppart)
+ppart <- prediction3(curepart, "twodem5", c(0, 1), "spop")
+plot(ppart)
+ppart <- prediction3(curepart, "twodem5", c(0, 1), "suncure")
+plot(ppart)
+
+ppart <- prediction3(curepart, "contiguity", c(0, 1), "uncureprob")
+plot(ppart)
+ppart <- prediction3(curepart, "contiguity", c(0, 1), "spop")
+plot(ppart)
+
+ppart <- prediction3(curepart, "tie", c(0, 1), "uncureprob")
+plot(ppart)
+ppart <- prediction3(curepart, "tie", c(0, 1), "spop")
+plot(ppart)
+ppart <- prediction3(curepart, "tie", c(0, 1), "suncure")
+plot(ppart)
+
