e1715f1602d9c44090521b31a374c12208baa7d9
George Williford <31017813+gwilliford@users.noreply.github.com>
2020-05-15 17:28:36 -0400

    commit before cleanup

--- V10 Replication/LHR20200409.R
+++ V10 Replication/LHR20200409.R
@@ -16,6 +16,7 @@ lhr$capchangelnt <- lhr$capchange * lhr$lnt
 lhr$samereg <- ifelse(lhr$twodem5 == 0 & lhr$onedem5 == 0, 1, 0)
 lhr$battletidelnt <- lhr$battletide * lhr$lnt
 lhr$twoaut5 = ifelse(lhr$twodem5 == 0 & lhr$onedem5 == 0, 1, 0)
+lhr$twodem5t = lhr$twodem5 * lhr$stop
 ##### Models ---------------------------------------------------------------------------
 
 ##### Coxph diagnostics
@@ -38,150 +39,107 @@ cox <- coxph(Surv(start, stop, event) ~ archigosFIRC + capchange + battletide +
                  data = lhr,
                  x = T); summary(cox)
 cox.zph(cox)
-cox2 <- coxph(Surv(start, stop, event) ~ archigosFIRC + capchange + battletide +
-               thirdpartycfire + index + twoaut5 + twodem5 + twodem5lnt + tie + lndeaths +
+cox2 <- coxph(Surv(start, stop, event) ~ capchange + capchangelnt + battletide +
+               thirdpartycfire + index + twoaut5 + twodem5 + tie + lndeaths +
                cfhist + cfhistlnt + stakes + contiguity,
              data = lhr,
              x = T); summary(cox2)
 #### cf hist --- capchange -- archigosFIRC ----- tie(ish)
+cox.zph(cox2, function(x) {log(x)})
 
 
-e <- tvcure(Surv(start, stop, event) ~ capchange + contiguity +
+cure1 <- tvcure(Surv(start, stop, event) ~ capchange + contiguity +
               stakes + cfhist + thirdpartycfire,
             cureform = ~ tie + battletide + lndeaths + thirdpartycfire + stakes +
               index + cfhist + twodem5 + twoaut5 + archigosFIRC,
             data = lhr,
             var = T, nboot = 30,
-            brglm = T); summary(e)
+            brglm = T); summary(cure1)
 
-a <- tvcure(Surv(start, stop, event) ~ capchange + capchangelnt + contiguity +
+cure2 <- tvcure(Surv(start, stop, event) ~ capchange + capchangelnt + contiguity +
               stakes + cfhist + cfhistlnt + thirdpartycfire,
             cureform = ~ tie + battletide + lndeaths + thirdpartycfire + stakes +
-              index + cfhist + twodem5 + twoaut5 + archigosFIRC,
+              index + cfhist + twodem5 + twoaut5,
             data = lhr,
             var = T, nboot = 30,
-            brglm = T); summary(a)
+            brglm = T); summary(cure2)
 b <- prediction3(a, variable = "twodem5", values = c(0, 1), type = "uncureprob", CI = F)
 
 
 
-##### set.seed
-# Cox model - full
-# cox <- coxph(Surv(start, stop, event) ~ lndeaths + tie + battletide + thirdpartycfire +
-#                stakes + onedem5 + twodem5 + index + cfhist + archigosFIRC +
-#                contiguity + capchange, data = lhr, x = T); summary(cox)
-# saveRDS(coxtab, file = "./res/coxtab.RDS")
-#
-#
-# # coxa <- coxph(Surv(start, stop, event) ~ lndeaths + tie + battletide + thirdpartycfire +
-#                stakes + onedem5 + index +
-#                contiguity, data = lhr, x = T); summary(cox); cox.zph(coxa)
-# coxb <- coxph(Surv(start, stop, event) ~ lndeaths + tie + battletide + thirdpartycfire + archigosFIRC  +
-#                stakes + samereg + index +
-#                contiguity, data = lhr, x = T); summary(coxb); cox.zph(coxb)
-# coxc <- coxph(Surv(start, stop, event) ~ lndeaths + tie + battletide + thirdpartycfire +
-#                stakes + samereg + index +
-#                contiguity, data = lhr, x = T); cox.zph(coxc)
-#
-# #- no ondem5, twodem5, cfhist, capchange
-# cureph2 <- tvcure(Surv(start, stop, event) ~  lndeaths + tie + stakes + battletide + thirdpartycfire + index + contiguity,
-#                  cureform = ~ lndeaths + battletide + thirdpartycfire +
-#                    tie + stakes + archigosFIRC + samereg + index +
-#                    contiguity,
-#                  data = lhr, var = T, nboot = 10, brglm = T); summary(cureph2)
-#
+####
 
-# Cure model - full
-# curefull <- tvcure(Surv(start, stop, event) ~  capchange + lndeaths +
-#                      battletide + tie + thirdpartycfire + stakes +
-#                      twodem5 + onedem5 + index + cfhist + contiguity,
-#              cureform = ~ archigosFIRC + capchange + lndeaths + battletide +
-#                tie + thirdpartycfire + stakes + onedem5 + twodem5 + index +
-#                cfhist + contiguity,
-#              data = lhr, var = T, nboot = 1000, brglm = T); summary(curefull)
-# saveRDS(curefull, file = "./res/curefull.RDS")
-# curefull2 <- tvcure(Surv(start, stop, event) ~  capchange + lndeaths +
-#                      battletide + tie + thirdpartycfire + stakes +
-#                      twodem5 + onedem5 + index + cfhist + contiguity,
-#                    cureform = ~ capchange + lndeaths + battletide +
-#                      tie + thirdpartycfire + stakes + onedem5 + twodem5 + index +
-#                      cfhist + contiguity,
-#                    data = lhr, var = T, nboot = 100, brglm = T); summary(curefull2)
-#
-# cureph <- tvcure(Surv(start, stop, event) ~  capchange + lndeaths +
-#                       battletide + tie + thirdpartycfire + stakes +
-#                       twodem5 + onedem5 + index + cfhist + contiguity,
-#                     cureform = ~ capchange + lndeaths + battletide +
-#                       tie + thirdpartycfire + stakes + onedem5 + twodem5 + index +
-#                       cfhist + contiguity,
-#                     data = lhr, var = T, nboot = 100, brglm = T); summary(curefull2)
+c1 <- survfit(cox2)
 
-# # Cure model - partial
-# curepart <- tvcure(Surv(start, stop, event) ~  capchange + index +
-#                      stakes + cfhist + contiguity,
-#              cureform = ~ archigosFIRC + capchange + lndeaths + battletide +
-#                tie + thirdpartycfire + stakes + onedem5 + twodem5 + index +
-#                cfhist + contiguity,
-#              data = lhr, var = T, nboot = 1000, brglm = T); summary(curepart)
-# saveRDS(curepart, file = "./res/curepart.RDS")
-#
-# cl <- makeCluster(3, "SOCK"); registerDoSNOW(cl)
-# curepart2 <- tvcure(Surv(start, stop, event) ~  capchange + index +
-#                      stakes + cfhist + contiguity + thirdpartycfire,
-#                    cureform = ~ archigosFIRC + lndeaths + battletide +
-#                      tie + stakes + onedem5 + twodem5 + index +
-#                      cfhist + contiguity,
-#                    data = lhr, var = T, nboot = 100, brglm = T); summary(curepart2)
-# curepart3 <- tvcure(Surv(start, stop, event) ~  capchange + index +
-#                       stakes + cfhist + contiguity + thirdpartycfire,
-#                     cureform = ~ archigosFIRC + lndeaths + battletide +
-#                       tie + stakes + onedem5 + twodem5 + (index > 0) + index +
-#                       cfhist + contiguity,
-#                     data = lhr, var = T, nboot = 100, brglm = T); summary(curepart3)
-# curepart4 <- tvcure(Surv(start, stop, event) ~  capchange + strata(tie) + index +
-#                       stakes + cfhist + contiguity + thirdpartycfire,
-#                     cureform = ~ archigosFIRC + lndeaths + battletide +
-#                       tie + stakes + onedem5 + twodem5 + (index > 0) +
-#                       cfhist + contiguity,
-#                     data = lhr, var = T, nboot = 100, brglm = T); summary(curepart4)
-#
-#
-# curepart5 <- tvcure(Surv(start, stop, event) ~  capchange + (index > 0) +
-#                       stakes + cfhist + contiguity + thirdpartycfire,
-#                     cureform = ~ archigosFIRC + lndeaths + battletide +
-#                       tie + stakes + onedem5 + twodem5 + (index > 0) +
-#                       cfhist + contiguity,
-#                     data = lhr, var = T, nboot = 100, brglm = T); summary(curepart5)
-#
-# curepart6 <- tvcure(Surv(start, stop, event) ~  (index > 0) + twodem5,
-#                     cureform = ~ archigosFIRC + lndeaths + battletide +
-#                       tie + stakes + onedem5 + twodem5 +
-#                       cfhist + contiguity,
-#                     data = lhr, var = T, nboot = 100, brglm = T); summary(curepart6)
-# curepart6 <- tvcure(Surv(start, stop, event) ~  index + twodem5 + lndeaths,
-#                     cureform = ~ lndeaths + battletide +
-#                       tie + stakes + onedem5 + twodem5 +
-#                       cfhist + contiguity,
-#                     data = lhr, var = T, nboot = 100, brglm = T); summary(curepart6)
+##### PLOTS --------------------------------------------------------------------
+
+### Tie variable
+# Cox pred
+newdata1 <- apply(cox2$x, 2, median, na.rm = T)
+newdata1 <- as.data.frame(rbind(newdata1, newdata1))
+newdata1[2, "tie"] <- 1
+pcox <- survfit(cox, newdata1, conf.int = 0.90)
+plot(pcox, T, col = c(1, 2))
+
+# Surv uncure
+tpred1 <- prediction4(cure2, "tie", c(0, 1), type = "uncureprob")
+plot(tpred1)
+
+# Surv cure
+tpred2 <- prediction4(cure2, "tie", c(0, 1), type = "spop")
+plot(tpred2)
+
+### Deaths variable
+# Cox pred
+lnmin <- min(lhr$lndeaths, na.rm = T)
+lnmax <- max(lhr$lndeaths, na.rm = T)
+newdata2 <- apply(cox2$x, 2, median, na.rm = T)
+newdata2 <- as.data.frame(rbind(newdata2, newdata2))
+newdata2[2, "lndeaths"] <- lnmin
+newdata2[2, "lndeaths"] <- lnmax
+pcox <- survfit(cox2, newdata2, conf.int = 0.90)
+plot(pcox, T, col = c(1, 2))
+
+dpred1 <- prediction4(cure2, "lndeaths", c(lnmin, lnmax), type = "uncureprob")
+plot(dpred1)
+dpred2 <- prediction4(cure2, "lndeaths", c(lnmin, lnmax), type = "spop", legendtitle = "Log Battle Deaths")
+plot(dpred2)
+
+### Battle consistency variable
+newdata3 <- apply(cox2$x, 2, median, na.rm = T)
+newdata3 <- as.data.frame(rbind(newdata3, newdata3))
+newdata3[1, "battletide"] <- 0
+newdata3[2, "battletide"] <- 1
+pcox <- survfit(cox2, newdata3, conf.int = 0.90)
+plot(pcox, T, col = c(1, 2))
+
+bpred1 <- prediction4(cure2, "battletide", c(0, 1), type = "uncureprob")
+plot(bpred1)
+bpred2 <- prediction4(cure2, "battletide", c(0, 1), type = "spop", legendtitle = "Battle Consistency")
+plot(bpred2)
 
 ##### Tables --------------------------------------------------------------------
 vl <- list("Battle Deaths" = "lndeaths",
      "Tie" = "tie",
-     "Battle Tide" = "battletide",
+     "Battle Consistency" = "battletide",
      "Third Party Intervention" = "thirdpartycfire",
      "Capability Change" = "capchange",
+     "Capability Change $\\times \\ln(t)$" = "capchangelnt",
      "Existential Stakes" = "stakes",
      "Agreement Strength" = "index",
      "Foreign-Imposed Regime Change" = "archigosFIRC",
      "Mixed Regime Type" = "onedem5",
      "Joint Democracy" = "twodem5",
      "Conflict History" = "cfhist",
+     "Conflict History $\\times \\ln(t)$" = "cfhistlnt",
      "Contiguity" = "contiguity")
-parttab <- tvtable_tvcure(curepart, format = "long", varlist = vl)
-fulltab <- tvtable_tvcure(curefull, format = "long", varlist = vl)
-coxtab <- tvtable_coxph(cox, format = "long", varlist = vl)
+# parttab <- tvtable_tvcure(curepart, format = "long", varlist = vl)
+# fulltab <- tvtable_tvcure(curefull, format = "long", varlist = vl)
+# coxtab <- tvtable_coxph(cox, format = "long", varlist = vl)
 
+tvtable(cox2, cure2)
 
+# tvtable_tvcure(curepart, format = "long", varlist = vl)
 
 # Combined Table
 combtab <- tvtable_combine(c("coxtab", "parttab"))
@@ -212,119 +170,6 @@ print(xtable(combtab3, align = "llXXXXX"),
       hline.after = getOption("xtable.hline.after", c(-1,0,nrow(x))),
 ) #file = "./tab/combtab.tex")
 
-
-# Footnote - pvalues/FIRC stands for foreign imposed regime change.
-
-# Good list
-tvtable.coxph(cox, format = "long",
-        varlist = list("Battle Deaths" = "lndeaths",
-                       "Tie" = "tie",
-                       "Battle Tide" = "battletide",
-                       "Third Party Intervention" = "thirdpartycfire",
-                       "Capability Change" = "capchange",
-                       "Existential Stakes" = "stakes",
-                       "Agreement Strength" = "index",
-                       "Foreign-Imposed Regime Change" = "archigosFIRC",
-                       "Mixed Regime Type" = "onedem5",
-                       "Joint Democracy" = "twodem5",
-                       "Conflict History" = "cfhist",
-                       "Contiguity" = "contiguity")
-)
-
-
-
-
-# Bad List
-tvtable.coxph(cox, varlist = list("$\\ln$ Battle Deaths" = "lndeaths",
-  "Tie" = "tie",
-  "Battle Tide" = "battletide",
-  "Third Party Intervention" = "thirdpartycfire",
-  "Capability Change",
-  "Existential Stakes" = "stakes",
-  "Agreement Strength" = "agree",
-  "Mixed Regime Type" = "onedem5",
-  "Joint Democracy" = "twodem5",
-  "Conflict History" = "cfhist",
-  "Contiguity" = "contiguity",
-  "Intercept"))
-
-# Shortlist
-tvtable.coxph(cox, format = "long",
-              varlist = list("Battle Deaths" = "lndeaths",
-                             "Tie" = "tie",
-                             "Battle Tide" = "battletide",
-                             "Foreign-Imposed Regime Change" = "archigosFIRC",
-                             "Mixed Regime Type" = "onedem5",
-                             "Joint Democracy" = "twodem5",
-                             "Conflict History" = "cfhist",
-                             "Contiguity" = "contiguity")
-)
-
-
-# Good list
-tvtable(curepart, format = "long",
-              varlist = list("Battle Deaths" = "lndeaths",
-                             "Tie" = "tie",
-                             "Battle Tide" = "battletide",
-                             "Third Party Intervention" = "thirdpartycfire",
-                             "Capability Change" = "capchange",
-                             "Existential Stakes" = "stakes",
-                             "Agreement Strength" = "index",
-                             "Foreign-Imposed Regime Change" = "archigosFIRC",
-                             "Mixed Regime Type" = "onedem5",
-                             "Joint Democracy" = "twodem5",
-                             "Conflict History" = "cfhist",
-                             "Contiguity" = "contiguity")
-)
-
-# Bad list
-tvtable(curepart, format = "long",
-        varlist = list("$\\ln$ Battle Deaths" = "lndeaths",
-                       "Tie" = "tie",
-                       "Battle Tide" = "battletide",
-                       "Third Party Intervention" = "thirdpartycfire",
-                       "Capability Change",
-                       "Existential Stakes" = "stakes",
-                       "Agreement Strength" = "agree",
-                       "Mixed Regime Type" = "onedem5",
-                       "Joint Democracy" = "twodem5",
-                       "Conflict History" = "cfhist",
-                       "Contiguity" = "contiguity",
-                       "Intercept")
-)
-
-
-# Bad list 2
-tvtable(curepart, format = "long",
-        varlist = list("Battle Deaths" = "lndeaths",
-                       "Tie" = "tie",
-                       "Battle Tide" = "battletide",
-                       "Third Party Intervention" = "thirdpartycfire",
-                       "Capability Change" = "capchange",
-                       "Existential Stakes" = "stakes",
-                       "Agreement Strength" = "index",
-                       "Foreign-Imposed Regime Change" = "archigosFIRC",
-                       "Mixed Regime Type" = "onedem5",
-                       "Joint Democracy" = "twodem5",
-                       "Conflict History" = "cfhist",
-                       "Contiguity" = "contiguity",
-                       "alpha" = "alpha")
-)
-
-tvtable(curepart, format = "long",
-        varlist = list("$\\ln$ Battle Deaths" = "lndeaths",
-                       "Tie" = "tie",
-                       "Battle Tide" = "battletide",
-                       "Third Party Intervention" = "thirdpartycfire",
-                       "Capability Change",
-                       "Existential Stakes" = "stakes",
-                       "Agreement Strength" = "agree",
-                       "Mixed Regime Type" = "onedem5",
-                       "Joint Democracy" = "twodem5",
-                       "Conflict History" = "cfhist",
-                       "Contiguity" = "contiguity",
-                       "Intercept")
-)
 
 newdata <- apply(cox$x, 2, median, na.rm = T)
 newdata <- as.data.frame(rbind(newdata, newdata))
