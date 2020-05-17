72d8a4df5b4aafbf077fe1b7731a2b17cd9cd755
George Williford <31017813+gwilliford@users.noreply.github.com>
2020-05-16 23:41:15 -0400

    Update V10 replication.
    Add cure plots.

--- V10 Replication/LHR20200409.R
+++ V10 Replication/LHR20200409.R
@@ -56,7 +56,8 @@ cn <- c("time", "surv", "lower", "upper", "num")
 breaks <- seq(0, 150, 10) * 365
 labs <- seq(0, 150, 10)
 
-### Baseline hazard - cox
+### Baseline hazard Comparison -------------------------------------------
+#cox basesurv
 coxbase <- survfit(cox2)
 coxbasepdat <- as.data.frame(cbind(coxbase$time, coxbase$surv, coxbase$lower, coxbase$upper))
 colnames(coxbasepdat) <- cn[-5]
@@ -66,35 +67,33 @@ coxbase <- ggplot(mapping = aes(x = time, y = surv), data = coxbasepdat) +
         scale_x_continuous(breaks = breaks, labels = labs) +
         theme(panel.border = element_rect(colour = "black", fill = NA)) + ylim(0, 1)
 
+#cure basesurv
 curebase <- prediction4(cure2, "tie", c(0, 1), type = "basesurv")
 curebase <- curebase + scale_x_continuous(breaks = breaks, labels = labs) +
         xlab("Time in years") + ylab("Probability of Survival") +
         theme(axis.title.y=element_blank())
 
+# combine
 base <- grid.arrange(coxbase, curebase + theme(axis.title.y=element_blank()), ncol = 2); base
 
-###
-#
-# newdata1 <- apply(cox2$x, 2, median, na.rm = T)
-# newdata1 <- as.data.frame(rbind(newdata1, newdata1))
-# newdata1[2, "tie"] <- 1
-# newdata1[2, "contiguity"] <- 1
-# newdata1[2, "twodem5"] <- 1
-# pcox1a <- survfit(cox2, newdata = newdata1, se.fit = T)
-
-
-### Tie variable
-# Cox pred
-# newdata1[1:12] <- 0
-# newdata2 <- newdata1
-# newdata2[13] <- 1
-# newdata1 <- 0
-# colnames(pdat1) <- cn
-# colnames(pdat2) <- cn
-library(gridExtra)
+### Tie comparison ---------------------------------------------------------------
 legendtitle <- "Tie"
 vals <- c(0, 1)
-a <- ggplot(mapping = aes(x = time, y = surv, col = as.factor(num), linetype = as.factor(num)), data = pdat) +
+
+# Cox plot
+newdata1 <- apply(cox2$x, 2, median, na.rm = T)
+newdata1 <- as.data.frame(rbind(newdata1, newdata1))
+newdata1[2, "tie"] <- 1
+pcoxtie1 <- survfit(cox2, newdata = newdata1[1, ], se.fit = T)
+pcoxtie2 <- survfit(cox2, newdata = newdata1[2, ], se.fit = T)
+
+pcoxtie1pdat <- cbind(as.data.frame(cbind(pcoxtie1$time, pcoxtie1$surv, pcoxtie1$lower, pcoxtie1$upper)), 1)
+pcoxtie2pdat <- cbind(as.data.frame(cbind(pcoxtie2$time, pcoxtie2$surv, pcoxtie2$lower, pcoxtie2$upper)), 2)
+colnames(pcoxtie1pdat) <- cn
+colnames(pcoxtie2pdat) <- cn
+pcoxtiepdat <- rbind(pcoxtie1pdat, pcoxtie2pdat)
+
+pcoxtieplot <- ggplot(mapping = aes(x = time, y = surv, col = as.factor(num), linetype = as.factor(num)), data = pcoxtiepdat) +
         geom_line() + geom_ribbon(aes(ymin = lower, ymax = upper,
                                       col = as.factor(num), linetype = as.factor(num), fill = as.factor(num)), alpha = 0.2) +
         labs(fill = legendtitle, linetype = legendtitle, col = legendtitle) +
@@ -103,10 +102,82 @@ a <- ggplot(mapping = aes(x = time, y = surv, col = as.factor(num), linetype = a
         scale_fill_discrete(labels = vals) +
         ylab("Probability of Survival") + xlab("Time in years") +
         scale_x_continuous(breaks = breaks, labels = labs) +
-        theme(panel.border = element_rect(colour = "black", fill = NA)) + theme_pubr()
+        theme(panel.border = element_rect(colour = "black", fill = NA))
+
+# Cureplot
+tpred1 <- prediction4(cure2, "tie", c(0, 1), type = "uncureprob")
+tplot1 <- tpred1 + xlab("Tie")
+tpred2 <- prediction4(cure2, "tie", c(0, 1), type = "spop", legendtitle = "Tie")
+tplot2 <- tpred2 + xlab("Time in years") + theme(legend.position = "none") +  ylab("Probability of Survival") + xlab("Time in years") + scale_x_continuous(breaks = breaks, labels = labs)
+
+# combine
+legend <- get_legend(pcoxtieplot)
+pcoxtieplot <- pcoxtieplot + theme(legend.position = "none")
+b <- grid.arrange(
+        grobs = list(legend, pcoxtieplot, tplot1, tplot2),
+        ncol = 2, nrow = 2,
+        # widths = c(0.1, 1, 1),
+        layout_matrix = rbind(c(1, 2),
+                              c(3, 4))
+)
+
+#save
+ggsave("C:/Users/gwill/Dropbox/Research/Dissertation/Manuscript/chapter2/img/tie.png", plot = b, width = 5.5, height = 4, units = "in")
+
+
+### Deaths comparison
+legendtitle <- "Log(Battle Deaths)"
+deathmin <- round(min(lhr$lndeaths, na.rm = T), 1)
+deathmax <- round(max(lhr$lndeaths, na.rm = T), 1)
+vals <- c(deathmin, deathmax)
+
+# Cox plot
+newdata2 <- apply(cox2$x, 2, median, na.rm = T)
+newdata2 <- as.data.frame(rbind(newdata2, newdata2))
+newdata2[1, "lndeaths"] <- deathmin
+newdata2[2, "lndeaths"] <- deathmax
+pcoxdeaths1 <- survfit(cox2, newdata = newdata2[1, ], se.fit = T)
+pcoxdeaths2 <- survfit(cox2, newdata = newdata2[2, ], se.fit = T)
+
+pcoxdeaths1pdat <- cbind(as.data.frame(cbind(pcoxdeaths1$time, pcoxdeaths1$surv, pcoxdeaths1$lower, pcoxdeaths1$upper)), 1)
+pcoxdeaths2pdat <- cbind(as.data.frame(cbind(pcoxdeaths2$time, pcoxdeaths2$surv, pcoxdeaths2$lower, pcoxdeaths2$upper)), 2)
+colnames(pcoxdeaths1pdat) <- cn
+colnames(pcoxdeaths2pdat) <- cn
+pcoxdeathspdat <- rbind(pcoxdeaths1pdat, pcoxdeaths2pdat)
+
+pcoxdeathsplot <- ggplot(mapping = aes(x = time, y = surv, col = as.factor(num), linetype = as.factor(num)), data = pcoxdeathspdat) +
+        geom_line() + geom_ribbon(aes(ymin = lower, ymax = upper,
+                                      col = as.factor(num), linetype = as.factor(num), fill = as.factor(num)), alpha = 0.2) +
+        labs(fill = legendtitle, linetype = legendtitle, col = legendtitle) +
+        scale_linetype_discrete(labels = vals)  +
+        scale_color_discrete(labels = vals) +
+        scale_fill_discrete(labels = vals) +
+        ylab("Probability of Survival") + xlab("Time in years") +
+        scale_x_continuous(breaks = breaks, labels = labs) +
+        theme(panel.border = element_rect(colour = "black", fill = NA))
+
+# Cureplot
+tpred1 <- prediction4(cure2, "lndeaths", c(deathmin, deathmax), type = "uncureprob")
+tplot1 <- tpred1 + xlab("Log(Battle Deaths)")
+tpred2 <- prediction4(cure2, "lndeaths", c(deathmin, deathmax), type = "spop", legendtitle = "Log(Battle Deaths)")
+tplot2 <- tpred2 + xlab("Time in years") + theme(legend.position = "none") +  ylab("Probability of Survival") + xlab("Time in years") + scale_x_continuous(breaks = breaks, labels = labs)
+
+# combine
+legend <- get_legend(pcoxdeathsplot)
+pcoxdeathsplot <- pcoxdeathsplot + theme(legend.position = "none")
+d <- grid.arrange(
+        grobs = list(legend, pcoxdeathsplot, tplot1, tplot2),
+        ncol = 2, nrow = 2,
+        # widths = c(0.1, 1, 1),
+        layout_matrix = rbind(c(1, 2),
+                              c(3, 4))
+)
+ggsave("C:/Users/gwill/Dropbox/Research/Dissertation/Manuscript/chapter2/img/deaths.png", plot = d, width = 5.5, height = 4, units = "in")
+
+
+
+
 # ggarrange(a, tplot1, tplot2, ncol = 2, nrow = 2)
-legend <- get_legend(a)
-a <- a + theme(legend.position = "none")
 # b <- grid.arrange(
 #         grobs = list(a, tplot1, tplot2),
 #         ncol = 2, nrow = 2,
@@ -114,13 +185,6 @@ a <- a + theme(legend.position = "none")
 #         layout_matrix = rbind(c(1, 2),
 #                               c(1, 3))
 # )
-b <- grid.arrange(
-        grobs = list(legend, a, tplot1, tplot2),
-        ncol = 2, nrow = 2,
-        # widths = c(0.1, 1, 1),
-        layout_matrix = rbind(c(1, 2),
-                              c(3, 4))
-)
 
 
 # ) + plot.background()
@@ -137,12 +201,9 @@ plot(pcox1b)
 autoplot(pcox1a)
 
 # Surv uncure
-tpred1 <- prediction4(cure2, "tie", c(0, 1), type = "uncureprob")
 tplot1 <- tpred1 + xlab("Tie")
 
 # Surv cure
-tpred2 <- prediction4(cure2, "tie", c(0, 1), type = "spop", legendtitle = "Tie")
-tplot2 <- tpred2 + xlab("Time in years") + theme(legend.position = "none") +  ylab("Probability of Survival") + xlab("Time in years") + scale_x_continuous(breaks = breaks, labels = labs)
 + ylim(0.5, 1)
 
 # Combine and save
