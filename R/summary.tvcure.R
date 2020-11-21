6af5c6432a6e6cad30ab36a41e082ce704a72ce2
George Williford <31017813+gwilliford@users.noreply.github.com>
2020-11-21 02:54:23 -0600

    Update summary.tvcure.R
    Commit to revert

--- R/summary.tvcure.R
+++ R/summary.tvcure.R
@@ -1,28 +1,28 @@
-summary.tvcure <- function(object) {
-  o <- object
-  if (object$var == T) {
-    gmat <- cbind(o$parametersgamma, o$parametersg_sd, o$parametersg_zvalue, o$parametersg_pvalue)
-    bmat <- cbind(o$parametersbeta, o$parametersb_sd, o$parametersb_zvalue, o$parametersb_pvalue)
-    rownames(gmat) <- o$varnames$gnames
-    rownames(bmat) <- o$varnames$bnames
-    colnames(gmat) <- c("GLM Coefficients", "Standard Error", "Z-Score", "p-value")
-    colnames(bmat) <- c("Hazard Coefficients", "Standard Error", "Z-Score", "p-value")
-    o$nboot
-    o$bootcomp
-  } else {
-    gmat <- as.matrix(o$parametersgamma)
-    bmat <- as.matrix(o$parametersbeta)
-    rownames(gmat) <- o$varnames$gnames
-    rownames(bmat) <- o$varnames$bnames
-    colnames(gmat) <- "GLM Coefficients"
-    colnames(bmat) <- "Hazard Coefficients"
-  }
-
-  cat("Call:\n")
-  print(o$call); cat("", sep="\n\n")
-  print(gmat)  ; cat("", sep="\n\n")
-  print(bmat)  ; cat("", sep="\n\n")
-  cat("n =", o$nobs, "with", o$nfail, "failures\n", sep = " ")
-  cat("Model converged in", o$emrun, "iterations\n", sep = " ")
-  if (o$options$var == T) cat("Variance estimates based on", o$bootcomp, "successful boostrap replications\n", sep = " ")
-}
+# summary.tvcure <- function(object) {
+#   o <- object
+#   if (object$var == T) {
+#     gmat <- cbind(o$parametersgamma, o$parametersg_sd, o$parametersg_zvalue, o$parametersg_pvalue)
+#     bmat <- cbind(o$parametersbeta, o$parametersb_sd, o$parametersb_zvalue, o$parametersb_pvalue)
+#     rownames(gmat) <- o$varnames$gnames
+#     rownames(bmat) <- o$varnames$bnames
+#     colnames(gmat) <- c("GLM Coefficients", "Standard Error", "Z-Score", "p-value")
+#     colnames(bmat) <- c("Hazard Coefficients", "Standard Error", "Z-Score", "p-value")
+#     o$nboot
+#     o$bootcomp
+#   } else {
+#     gmat <- as.matrix(o$parametersgamma)
+#     bmat <- as.matrix(o$parametersbeta)
+#     rownames(gmat) <- o$varnames$gnames
+#     rownames(bmat) <- o$varnames$bnames
+#     colnames(gmat) <- "GLM Coefficients"
+#     colnames(bmat) <- "Hazard Coefficients"
+#   }
+#
+#   cat("Call:\n")
+#   print(o$call); cat("", sep="\n\n")
+#   print(gmat)  ; cat("", sep="\n\n")
+#   print(bmat)  ; cat("", sep="\n\n")
+#   cat("n =", o$nobs, "with", o$nfail, "failures\n", sep = " ")
+#   cat("Model converged in", o$emrun, "iterations\n", sep = " ")
+#   if (o$options$var == T) cat("Variance estimates based on", o$bootcomp, "successful boostrap replications\n", sep = " ")
+# }
