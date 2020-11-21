d5b81497fe706e205ecfa43fd4e41b10fa88fec3
George Williford <31017813+gwilliford@users.noreply.github.com>
2020-11-20 23:47:20 -0600

    Update summary.tvcure.R
    Update to reflect new structure of tvcure output.

--- R/summary.tvcure.R
+++ R/summary.tvcure.R
@@ -1,19 +1,19 @@
 summary.tvcure <- function(object) {
   o <- object
   if (object$var == T) {
-    gmat <- cbind(o$gamma, o$g_sd, o$g_zvalue, o$g_pvalue)
-    bmat <- cbind(o$beta, o$b_sd, o$b_zvalue, o$b_pvalue)
-    rownames(gmat) <- o$gnames
-    rownames(bmat) <- o$bnames
+    gmat <- cbind(o$parametersgamma, o$parametersg_sd, o$parametersg_zvalue, o$parametersg_pvalue)
+    bmat <- cbind(o$parametersbeta, o$parametersb_sd, o$parametersb_zvalue, o$parametersb_pvalue)
+    rownames(gmat) <- o$varnames$gnames
+    rownames(bmat) <- o$varnames$bnames
     colnames(gmat) <- c("GLM Coefficients", "Standard Error", "Z-Score", "p-value")
     colnames(bmat) <- c("Hazard Coefficients", "Standard Error", "Z-Score", "p-value")
     o$nboot
     o$bootcomp
   } else {
-    gmat <- as.matrix(o$gamma)
-    bmat <- as.matrix(o$beta)
-    rownames(gmat) <- o$gnames
-    rownames(bmat) <- o$bnames
+    gmat <- as.matrix(o$parametersgamma)
+    bmat <- as.matrix(o$parametersbeta)
+    rownames(gmat) <- o$varnames$gnames
+    rownames(bmat) <- o$varnames$bnames
     colnames(gmat) <- "GLM Coefficients"
     colnames(bmat) <- "Hazard Coefficients"
   }
@@ -24,5 +24,5 @@ summary.tvcure <- function(object) {
   print(bmat)  ; cat("", sep="\n\n")
   cat("n =", o$nobs, "with", o$nfail, "failures\n", sep = " ")
   cat("Model converged in", o$emrun, "iterations\n", sep = " ")
-  if (object$var == T) cat("Variance estimates based on", o$bootcomp, "successful boostrap replications\n", sep = " ")
+  if (o$options$var == T) cat("Variance estimates based on", o$bootcomp, "successful boostrap replications\n", sep = " ")
 }
