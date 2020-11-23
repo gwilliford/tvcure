3dee94748cbe4edbf1d6ba7335f7c29eedb7b731
George Williford <31017813+gwilliford@users.noreply.github.com>
2020-11-22 23:50:54 -0600

    Update tvresid.R
    Fix so it takes curemod betas instead of regular cox

--- R/tvresid.R
+++ R/tvresid.R
@@ -5,16 +5,16 @@ tvresid = function(model, type = c("Cox-Snell", "M-Cox-Snell", "Martingale", "M-
   if (class(model) != "tvcure") stop("Model must be a tvcure object")
 
   # Data
-  ttime = model$Time
-  cens = model$Status
+  Time = model$Time
+  Status = model$Status
   X = model$X
   Z = model$Z[, -1]
 
   uncureprob = model$uncureprob
   survprob = model$Survival
   weight = model$w
-  beta_uncure = model$uncuremod$coefficients
-  unculp = as.vector(X %*% beta_uncure)
+  beta = model$beta
+  cox_xb = as.vector(X %*% beta)
 
   # glm parameters
   gamma = model$gamma[-1]
@@ -22,31 +22,35 @@ tvresid = function(model, type = c("Cox-Snell", "M-Cox-Snell", "Martingale", "M-
   if (type == "Cox-Snell") {
     residuals = -log(uncureprob * survprob + 1 - uncureprob)
     resid.uncure = -log(survprob)
-    out = list(type = type, cens = cens, time = ttime, weight = model$w,
-         residuals = residuals, resid.uncure = resid.uncure)
+    out = list(type = type, Status = Status, time = Time, weight = model$w,
+         residuals = residuals, resid.uncure = resid.uncure,
+         X = X, Z = Z)
   }
   else if (type == "M-Cox-Snell") {
     residuals = -log(survprob)
-    resid.dist = (function(resids, cens, weight) {
+    resid.dist = (function(resids, Status, weight) {
       design = survey::svydesign(id = ~1, weights = weight[weight > 0])
-      survey::svykm(Surv(resids[weight > 0], cens[weight > 0]) ~ 1, design = design)
-    })(residuals, cens, weight)
-    out = list(type = type, cens = cens, time = ttime, weight = weight,
-         residuals = residuals, resid.dist = resid.dist)
+      survey::svykm(Surv(resids[weight > 0], Status[weight > 0]) ~ 1, design = design)
+    })(residuals, Status, weight)
+    out = list(type = type, Status = Status, time = Time, weight = weight,
+         residuals = residuals, resid.dist = resid.dist,
+         X = X, Z = Z)
   }
   else if (type == "M-Martingale") {
     keep = weight != 0
-    val = cens + weight * log(survprob)
-    val2 = residuals(model$uncuremod, weighted = T)
-    deviance = sign(val) * sqrt(-2 * (val + cens * log(cens - val)))
-    out = list(type = type, xm = X[keep, ], keep = keep, unculp = unculp[keep],
+    val = Status + weight * log(survprob)
+    val2 = residuals(model$curemod$latency_fit, weighted = T)
+    deviance = sign(val) * sqrt(-2 * (val + Status * log(Status - val)))
+    out = list(type = type, xm = X[keep, ], keep = keep, cox_xb = cox_xb[keep],
          residuals = val[keep], deviance = deviance[keep],
-         residuals2 = val2)
+         residuals2 = val2,
+         X = X, Z = Z)
   }
   else if (type == "Martingale") {
-    val = cens + log(uncureprob * (survprob - 1) + 1)
-    out = list(type = type, xm = X, unculp = unculp, residuals = val,
-         deviance = sign(val) * sqrt(-2 * (val + cens * log(cens - val))))
+    val = Status + log(uncureprob * (survprob - 1) + 1)
+    out = list(type = type, xm = X, cox_xb = cox_xb, residuals = val,
+         deviance = sign(val) * sqrt(-2 * (val + Status * log(Status - val))),
+         X = X, Z = Z)
   }
   return(out)
 }
