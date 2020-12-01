3f945cc507018cc719c77d9c6ef2f90848975a89
George Williford <31017813+gwilliford@users.noreply.github.com>
2020-11-30 18:59:21 -0600

    Move tvbh to its own file

--- R/tvresid_schoen.R
+++ R/tvresid_schoen.R
@@ -1,29 +1,3 @@
-tvbh = function(model) {
-  X = model$X
-  beta = model$beta
-  Time = model$Time
-  Status = model$Status
-
-  death_point <- sort(unique(subset(Time, Status == 1)))
-  coxexp = exp((beta) %*% t(X))
-
-  lambda <- numeric()
-  event <- numeric()
-
-  # Estimate the baseline hazard
-  for (i in 1:length(death_point)) {
-    # Total # deaths at deathpoint
-    event[i] <- sum(Status * as.numeric(Time == death_point[i]))
-    # risk of death for each individual in subset of individuals still alive (done by Time >= death_point)
-    temp <- sum(as.numeric(Time >= death_point[i]) * drop(coxexp))
-    temp1 <- event[i]
-    # no. events / total risk = baseline hazard
-    lambda[i] <- temp1/temp
-  }
-  # print(death_point)
-  # print(lambda)
-  return(lambda)
-}
 
 sch = function(model) {
   X = model$X
